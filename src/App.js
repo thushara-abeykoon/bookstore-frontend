cd155765a5fdcf2fb9c0e9a93c9254f51d9dae5d
Thushara Abeykoon <thushara.abeykoon99@gmail.com>
2024-05-30 08:32:50 +0530

    added route option to navigation bar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Dashboard from './component/Dashboard';
 import SideBar from './component/SideBar';
+import Authors from './component/Authors';
+import Books from './component/Books';
 
 function App() {
   return (
     <>
       <SideBar />
-      <Dashboard />
+      <Routes>
+        <Route path='/' element={<Dashboard />} />
+        <Route path='/authors' element={<Authors />} />
+        <Route path='/books' element={<Books />} />
+      </Routes>
     </>
   );
 }
--- src/component/NavigationBar.js
+++ src/component/NavigationBar.js
@@ -2,14 +2,24 @@ import React from 'react'
 import { FaHome } from "react-icons/fa";
 import { MdSupervisorAccount } from "react-icons/md";
 import { IoBookSharp } from "react-icons/io5";
+import { useLocation, useNavigate } from 'react-router-dom';
 
 const NavigationBar = () => {
+
+  const navigate = useNavigate();
+
+  const currentRoute = useLocation();
+
+  const handleNavigation = (path) => {
+    navigate(path);
+  }
+
   return (
     <div className='px-3 py-16'>
         <ul className='flex flex-col gap-4'>
-            <li className='navigation-list-item bg-black text-white'><FaHome />Home</li>
-            <li className='navigation-list-item'><MdSupervisorAccount />Authors</li>
-            <li className='navigation-list-item'><IoBookSharp />Books</li>
+            <li className={`navigation-list-item ${currentRoute.pathname==='/'?'navigation-list-item-active':''}`} onClick={() => navigate('/')}><FaHome />Home</li>
+            <li className={`navigation-list-item ${currentRoute.pathname==='/authors'?'navigation-list-item-active':''}`} onClick={() => navigate('/authors')}><MdSupervisorAccount />Authors</li>
+            <li className={`navigation-list-item ${currentRoute.pathname==='/books'?'navigation-list-item-active':''}`} onClick={() => navigate('/books')}><IoBookSharp />Books</li>
             <li></li>
         </ul>
     </div>
