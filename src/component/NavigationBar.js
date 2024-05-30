e423e73cf1f9dcd05c062c94d388103438ec0bed
Thushara Abeykoon <thushara.abeykoon99@gmail.com>
2024-05-30 09:47:33 +0530

    added add book and add author button

--- src/component/NavigationBar.js
+++ src/component/NavigationBar.js
@@ -3,7 +3,8 @@ import { FaHome } from "react-icons/fa";
 import { MdSupervisorAccount } from "react-icons/md";
 import { IoBookSharp } from "react-icons/io5";
 import { useLocation, useNavigate } from 'react-router-dom';
-import { TiUserAdd } from "react-icons/ti";
+import { HiUserAdd } from "react-icons/hi";
+import { BiSolidBookAdd  } from "react-icons/bi";
 
 const NavigationBar = () => {
 
@@ -20,7 +21,9 @@ const NavigationBar = () => {
         <ul className='flex flex-col gap-4'>
             <li className={getNavItemClass('/')} onClick={() => navigate('/')}><FaHome />Home</li>
             <li className={getNavItemClass('/authors')} onClick={() => navigate('/authors')}><MdSupervisorAccount />Authors</li>
+            {currentRoute.pathname==="/authors"?<li className='navigation-list-item hover:bg-white hover:bg-opacity-40'><HiUserAdd /> Add Author</li>:null}
             <li className={getNavItemClass('/books')} onClick={() => navigate('/books')}><IoBookSharp />Books</li>
+            {currentRoute.pathname==="/books"?<li className='navigation-list-item hover:bg-white hover:bg-opacity-40'><BiSolidBookAdd  /> Add Book</li>:null}
             <li></li>
         </ul>
     </div>
--- src/component/SideBar.js
+++ src/component/SideBar.js
@@ -6,8 +6,10 @@ import { IoMdLogOut } from "react-icons/io";
 const SideBar = () => {
   return (
     <div className='bg-red-100 w-1/6 px-4 h-screen flex flex-col justify-between pb-20 fixed left-0 top-0'>
-        <Header />
-        <NavigationBar />
+        <div>
+          <Header />
+          <NavigationBar />
+        </div>
         <button className='bg-black rounded-xl text-white flex items-center justify-start px-[50px] py-[10px] gap-4 uppercase font-bold text-lg'><IoMdLogOut className='text-2xl' />Logout</button>
     </div>
   )
