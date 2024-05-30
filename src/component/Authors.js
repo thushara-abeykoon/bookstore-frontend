ef3893eb6f0c96f84332f83182dc2a98b682b4ca
Thushara Abeykoon <thushara.abeykoon99@gmail.com>
2024-05-30 13:07:43 +0530

    added all authors section

--- src/component/Authors.js
+++ src/component/Authors.js
@@ -1,8 +1,34 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import AuthorCard from './AuthorCard'
+import axios from 'axios';
 
 const Authors = () => {
+  const [authors, setAuthors] = useState([]);
+
+  useEffect(() => {
+      const fetchAuthors = async () => {
+        await axios.get("http://localhost:8080/api/v1/author/getAll")
+        .then((response) => {
+          setAuthors(response.data);
+        })
+        .catch((err)=>{console.log(err);});
+      }
+      fetchAuthors();
+    },[]);
+
   return (
-    <div>Authors</div>
+    <div className='w-5/6 float-right px-10 py-10 flex flex-col gap-10'> 
+      <h3 className='text-5xl font-bold'>All Authors</h3>
+      <div className='grid-box'>
+        {authors.length!==0?
+          authors.map((author)=>{
+            return <AuthorCard key={author.id} firstName={author.firstName} lastName={author.lastName} bookCount={9} likeCount={10} />
+          })
+          :
+          <p>No authors found</p>
+        }
+      </div>
+    </div>
   )
 }
 
