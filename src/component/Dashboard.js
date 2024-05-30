0f9edf37577c02f3672cb7eca5827009779c4fe3
Thushara Abeykoon <thushara.abeykoon99@gmail.com>
2024-05-30 09:33:32 +0530

    fixed side bar to the left and added author card to the dashboard

--- src/component/Dashboard.js
+++ src/component/Dashboard.js
@@ -30,8 +30,40 @@ const Dashboard = () => {
             contactNo: "0987654321"
         }},
     ];
+
+    const authors = [
+        {
+            id: 1,
+            firstName: "John",
+            lastName: "Doe",
+            email: "doe@gmail.com",
+            contactNo: "1234567890"
+        },
+        {
+            id: 2,
+            firstName: "Kamal",
+            lastName: "Perera",
+            email: "perera@gmail.com",
+            contactNo: "0987654321"
+        },
+        {
+            id: 3,
+            firstName: "Nimal",
+            lastName: "Nishantha",
+            email: "nimal@outlook.com",
+            contactNo: "123456678"
+        },
+        {
+            id: 4,
+            firstName: "Nimal",
+            lastName: "Nishantha",
+            email: "nimal@outlook.com",
+            contactNo: "123456678"
+        },
+    ]
+
   return (
-    <div className='float-left w-5/6 px-10 py-10 flex flex-col gap-20 bg-gray-100'>
+    <div className='float-right w-5/6 px-10 py-10 flex flex-col gap-20 bg-gray-100'>
         <div className='flex-box gap-20'>
             <h2 className='text-5xl font-bold font-sans'>Welcome Admin!</h2>
             <SearchBar />
@@ -48,7 +80,9 @@ const Dashboard = () => {
         <div className='flex flex-col items-start gap-10'>
             <h3 className='font-bold text-xl uppercase'>Top Authors</h3>
             <div className='flex-box justify-start gap-10'>
-                <AuthorCard />
+                {authors.map(author=>(
+                    <AuthorCard key={author.id} firstName={author.firstName} lastName={author.lastName} likeCount={10} bookCount={20} />
+                ))}
             </div>
         </div>
 
