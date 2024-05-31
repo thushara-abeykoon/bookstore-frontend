027d3e8308051d7ae81c3f6c6dae5f59636b7a27
Thushara Abeykoon <thushara.abeykoon99@gmail.com>
2024-05-31 09:15:01 +0530

    changed author panel styles

--- src/component/AddAuthorPanel.js
+++ src/component/AddAuthorPanel.js
@@ -46,14 +46,14 @@ const AddAuthorPanel = () => {
     }
 
   return (
-    <div className='fixed right-0 top-0 h-screen w-1/4 shadow-2xl bg-white text-black p-4 flex flex-col items-center gap-10'>
-        <div className='text-xl w-full flex-box justify-end cursor-pointer'><ImCross onClick={()=>handleAddAuthorPanel(false)} /></div>
+    <div className='add-new-panel'>
+        <div className='text-xl w-full flex-box justify-end cursor-pointer text-white'><ImCross onClick={()=>handleAddAuthorPanel(false)} /></div>
         <img src={SampleUserImage} width={200} />
         <form onSubmit={handleAuthorSubmit} className='w-3/4 flex flex-col items-center gap-10 '>
-          <input ref={firstNameRef} type='text' className={textFieldStyle} placeholder='First Name' />
-          <input ref={lastNameRef} type='text' className={textFieldStyle} placeholder='Last Name' />
-          <input ref={emailRef} type='email' className={textFieldStyle} placeholder='Email' />
-          <input ref={contactNoRef} type='text' className={textFieldStyle} placeholder='Contact Number' />
+          <input ref={firstNameRef} type='text' className="add-new-panel-input" placeholder='First Name' />
+          <input ref={lastNameRef} type='text' className="add-new-panel-input" placeholder='Last Name' />
+          <input ref={emailRef} type='email' className="add-new-panel-input" placeholder='Email' />
+          <input ref={contactNoRef} type='text' className="add-new-panel-input" placeholder='Contact Number' />
           <button className=' w-full bg-black rounded-xl text-white flex items-center mt-7 justify-center px-[50px] py-[10px] gap-4 uppercase font-bold text-lg'>Add</button>  
         </form>
     </div>
---
+++ src/component/AddNewBookPanel.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AddNewBookPanel = () => {
+  return (
+    <div className='h-screen w-1/4'>AddNewBookPanel</div>
+  )
+}
+
+export default AddNewBookPanel
