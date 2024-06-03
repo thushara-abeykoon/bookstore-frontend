bd307d09fb7d4a67459ed31d8ad1352093a8f687
Thushara Abeykoon <thushara.abeykoon99@gmail.com>
2024-06-03 13:40:03 +0530

    added responsive design to dashboard

--- src/component/Dashboard.js
+++ src/component/Dashboard.js
@@ -16,13 +16,13 @@ const Dashboard = () => {
     
 
   return (
-    <div className='float-right w-5/6 px-10 py-10 flex flex-col gap-20'>
-        <div className='flex-box gap-20'>
+    <div className='md:float-right md:w-5/6 max-md:w-full px-10 py-10 flex flex-col gap-20'>
+        <div className='flex-box gap-20 max-md:flex-col-reverse max-md:gap-10'>
             <h2 className='text-5xl font-bold font-sans'>Welcome {login?"Admin":"User"}!</h2>
             <SearchBar />
         </div>
         <div className='flex flex-col w-full items-start gap-10'>
-            <h3 className='font-bold text-xl  uppercase'>Most Liked Books</h3>
+            <h3 className='font-bold text-xl uppercase'>Most Liked Books</h3>
             <div className='w-full grid-box'>
                 {mostLikedBooks?.map(book=>(
                     <BookCard key={book.isbn} book={book} />
@@ -54,8 +54,8 @@ const SearchBar = () => {
     }
 
   return (
-    <div onClick={()=>setSearchActive(false)} className={searchActive?'w-5/6 h-screen fixed top-0 right-0 bg-white bg-opacity-30 backdrop-blur-md px-10 py-10 flex flex-col gap-20':''}>
-        <div className={searchActive?'w-full flex-box border-b-2 px-10 py-3 border-black ':'border-2 border-gray-400 w-96 rounded-2xl py-2 px-5 flex-box'}>
+    <div onClick={()=>setSearchActive(false)} className={searchActive?'md:w-5/6 max-md:w-full h-screen fixed bottom-0 right-0 bg-white bg-opacity-30 backdrop-blur-md px-10 py-10 flex flex-col gap-20':'max-md:w-full'}>
+        <div className={searchActive?'w-full flex-box border-b-2 px-10 py-3 border-black ':'border-2 border-gray-400 w-full rounded-2xl py-2 px-5 flex-box'}>
           <input onChange={e=>{
             if (e.target.value.length!==0)
                 setSearchActive(true);
