30ad29a7ef5a8591389c41f6d843d5c653d10e3b
Thushara Abeykoon <thushara.abeykoon99@gmail.com>
2024-06-03 11:44:59 +0530

    added bookcount and likecount

--- src/component/author/AuthorCard.js
+++ src/component/author/AuthorCard.js
@@ -1,11 +1,37 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import UserImage from '../../assets/219970.png'
 import { FaBookOpen, FaHeart } from 'react-icons/fa';
 import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
-const AuthorCard = ({email, firstName, lastName, bookCount, likeCount}) => {
+const AuthorCard = ({email, firstName, lastName}) => {
 
     const navigate = useNavigate();
+    const [bookCount, setBookCount] = useState(0);
+    const [likeCount, setLikeCount] = useState(0);
+    
+    useEffect(()=>{
+      fetchBookCount(email);
+    })
+
+    const calculateTotalLikes = (books) => {
+      let tempLikeCount = 0;
+      books.forEach(book => {
+        tempLikeCount += book.likeCount;
+      });
+      setLikeCount(tempLikeCount);
+    }
+
+    const fetchBookCount = async(email) => {
+      await axios.get(`http://localhost:8080/api/v1/book/getByAuthor/${email}`)
+      .then(res=>{
+        setBookCount(res.data.length);
+        calculateTotalLikes(res.data);
+      })
+      .catch(err=>console.error(err));
+
+      
+    }
 
   return (
     <div className='bg-white w-64 shadow-xl rounded-lg pt-3 pb-10 flex flex-col items-center gap-4 px-5 cursor-pointer' onClick={() => navigate(`/authors/${email}`)} >
