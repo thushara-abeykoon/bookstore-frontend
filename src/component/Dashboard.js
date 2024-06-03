523853d7f292faf2e1c08e940b7936bdefbda37f
Thushara Abeykoon <thushara.abeykoon99@gmail.com>
2024-06-03 10:25:31 +0530

    added most liked books to the dashboard

--- src/component/Dashboard.js
+++ src/component/Dashboard.js
@@ -1,66 +1,50 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { FaSearch } from "react-icons/fa";
 import BookCard from './book/BookCard';
 import AuthorCard from './author/AuthorCard';
+import { AppContext } from '../App';
 
 const Dashboard = () => {
-    const books = [
-        {isbn: 1, title: 'Book 1', likeCount:15, author: {
-            firstName: "John",
-            lastName: "Doe",
-            email: "doe@gmail.com",
-            contactNo: "1234567890"
-        }},
-        {isbn: 2, title: 'Book 2', likeCount:2, author: {
-            firstName: "John",
-            lastName: "Doe",
-            email: "doe@gmail.com",
-            contactNo: "1234567890"
-        }},
-        {isbn: 3, title: 'Book 3', likeCount:5, author: {
-            firstName: "Kamal",
-            lastName: "Perera",
-            email: "perera@gmail.com",
-            contactNo: "0987654321"
-        }},
-        {isbn: 4, title: 'Book 4', likeCount:6, author: {
-            firstName: "Kamal",
-            lastName: "Perera",
-            email: "perera@gmail.com",
-            contactNo: "0987654321"
-        }},
-    ];
 
-    const authors = [
-        {
-            id: 1,
-            firstName: "John",
-            lastName: "Doe",
-            email: "doe@gmail.com",
-            contactNo: "1234567890"
-        },
-        {
-            id: 2,
-            firstName: "Kamal",
-            lastName: "Perera",
-            email: "perera@gmail.com",
-            contactNo: "0987654321"
-        },
-        {
-            id: 3,
-            firstName: "Nimal",
-            lastName: "Nishantha",
-            email: "nimal@outlook.com",
-            contactNo: "123456678"
-        },
-        {
-            id: 4,
-            firstName: "Nimal",
-            lastName: "Nishantha",
-            email: "nimal@outlook.com",
-            contactNo: "123456678"
-        },
-    ]
+    const {authors, books} = useContext(AppContext);
+    const [mostLikedBooks, setMostLikedBooks] = useState([]);
+    useEffect(()=>{
+        const sortedBooks = books;
+        sortedBooks.sort((a,b)=>b.likeCount- a.likeCount);
+        setMostLikedBooks(sortedBooks.slice(0,4));
+    },[])
+    
+
+    // const authors = [
+    //     {
+    //         id: 1,
+    //         firstName: "John",
+    //         lastName: "Doe",
+    //         email: "doe@gmail.com",
+    //         contactNo: "1234567890"
+    //     },
+    //     {
+    //         id: 2,
+    //         firstName: "Kamal",
+    //         lastName: "Perera",
+    //         email: "perera@gmail.com",
+    //         contactNo: "0987654321"
+    //     },
+    //     {
+    //         id: 3,
+    //         firstName: "Nimal",
+    //         lastName: "Nishantha",
+    //         email: "nimal@outlook.com",
+    //         contactNo: "123456678"
+    //     },
+    //     {
+    //         id: 4,
+    //         firstName: "Nimal",
+    //         lastName: "Nishantha",
+    //         email: "nimal@outlook.com",
+    //         contactNo: "123456678"
+    //     },
+    // ]
 
   return (
     <div className='float-right w-5/6 px-10 py-10 flex flex-col gap-20'>
@@ -71,7 +55,7 @@ const Dashboard = () => {
         <div className='flex flex-col items-start gap-10'>
             <h3 className='font-bold text-xl  uppercase'>Most Liked Books</h3>
             <div className='flex-box justify-start gap-10'>
-                {books.map(book=>(
+                {mostLikedBooks?.map(book=>(
                     <BookCard key={book.isbn} book={book} />
                 ))}
             </div>
