e745df82681d8724f21dc09edb50333acd474667
Thushara Abeykoon <thushara.abeykoon99@gmail.com>
2024-06-03 11:53:44 +0530

    removed dummy authors

--- src/component/Dashboard.js
+++ src/component/Dashboard.js
@@ -15,37 +15,6 @@ const Dashboard = () => {
     },[books])
     
 
-    // const authors = [
-    //     {
-    //         id: 1,
-    //         firstName: "John",
-    //         lastName: "Doe",
-    //         email: "doe@gmail.com",
-    //         contactNo: "1234567890"
-    //     },
-    //     {
-    //         id: 2,
-    //         firstName: "Kamal",
-    //         lastName: "Perera",
-    //         email: "perera@gmail.com",
-    //         contactNo: "0987654321"
-    //     },
-    //     {
-    //         id: 3,
-    //         firstName: "Nimal",
-    //         lastName: "Nishantha",
-    //         email: "nimal@outlook.com",
-    //         contactNo: "123456678"
-    //     },
-    //     {
-    //         id: 4,
-    //         firstName: "Nimal",
-    //         lastName: "Nishantha",
-    //         email: "nimal@outlook.com",
-    //         contactNo: "123456678"
-    //     },
-    // ]
-
   return (
     <div className='float-right w-5/6 px-10 py-10 flex flex-col gap-20'>
         <div className='flex-box gap-20'>
@@ -64,7 +33,7 @@ const Dashboard = () => {
         <div className='flex flex-col items-start gap-10'>
             <h3 className='font-bold text-xl uppercase'>Top Authors</h3>
             <div className='flex-box justify-start gap-10'>
-                {authors.map(author=>(
+                {authors.slice(0,4).map(author=>(
                     <AuthorCard key={author.id} email={author.email} firstName={author.firstName} lastName={author.lastName} likeCount={10} bookCount={20} />
                 ))}
             </div>
